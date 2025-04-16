f245e29e60e7410ca246586c1ee9787b0404314a
Al Nahian Hasan <nahianhasan161@gmail.com>
2025-04-16 23:10:07 +0600

    added google ads manager for tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
 import type { Metadata } from "next";
-
 import "./globals.css";
 import { metropolisFont } from "@/fonts/fonts";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Shpper",
   description: "A Shopping Site",
 };
+
 export const runtime = "edge";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -15,7 +17,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${metropolisFont.variable} `}>{children}</body>
+      <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-16827691070"
+         />
+
+        {/* Google Tag Manager Script */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'AW-16827691070');
+
+          `}
+        </Script>
+      </head>
+      <body className={`${metropolisFont.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
